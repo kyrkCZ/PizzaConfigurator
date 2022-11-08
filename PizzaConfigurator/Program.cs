076e9f230a5acdb98eb9851c6ef53c928aa2e58d
kyrkCZ <kramny.kubik@seznam.cz>
2022-11-08 17:07:14 +0100

    First commit

---
+++ PizzaConfigurator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PizzaConfigurator
+{
+    internal class Pizza
+    {
+        
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public void start()
+        {
+            Console.WriteLine();
+            int input = Console.Read();
+            switch (input)
+            {
+                case 1: break;
+            }
+        }
+    }
+}
