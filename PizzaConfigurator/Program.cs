d0a74ff9de50b3df1bff859f2b250ddddb710c51
kyrkCZ <kramny.kubik@seznam.cz>
2022-11-09 12:50:49 +0100

    User input check

--- PizzaConfigurator/Program.cs
+++ PizzaConfigurator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Mime;
+using System.Runtime.InteropServices;
 
 namespace PizzaConfigurator
 {
@@ -12,10 +13,18 @@ namespace PizzaConfigurator
             pizza.start();
         }
 
-        public void start()
+        private void start()
         {
-            Console.WriteLine();
-            int input = Console.Read();
+            Console.WriteLine("Vyberte možnost 1,2,3,4,5");
+            int input = 0;
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Zkuste to znovu");
+            }
             switch (input)
             {
                 case 1: 
@@ -24,13 +33,17 @@ namespace PizzaConfigurator
                     break;
                 case 2: 
                     //Oblibené pizzy
-                    
+
                     break;
-                case 3: 
-                    //Seznam ingrediencí
+                case 3:
+                    //Seznam předsestavených pizza
                     
                     break;
                 case 4: 
+                    //Seznam ingrediencí
+                    
+                    break;
+                case 5: 
                     //End
                     System.Environment.Exit(0);
                     break;
@@ -38,6 +51,11 @@ namespace PizzaConfigurator
                     start();
                     break;
             }
+        }
+
+        public void select()
+        {
+            
         }
     }
 }
