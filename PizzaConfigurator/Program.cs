72f92eaab3806cbdf1e25380580d7c190b4a4a99
kyrkCZ <kramny.kubik@seznam.cz>
2022-11-14 17:47:41 +0100

    Updated phase selector

--- PizzaConfigurator/Program.cs
+++ PizzaConfigurator/Program.cs
@@ -2,15 +2,14 @@
 
 namespace PizzaConfigurator
 {
-    class Program
+    static class Program
     {
         public static void Main(string[] args)
         {
-            Program program = new Program();
-            program.start();
+            Start();
         }
 
-        public void start()
+        public static void Start()
         {
             while (true)
             {
@@ -19,7 +18,7 @@ namespace PizzaConfigurator
                     case 1:
                         //Sestaveni pizza
                         Pizza pizza = new Pizza();
-                        pizza.selectBase();
+                        pizza.Phase(0);
                         break;
                     case 2:
                         //Oblibené pizzy
@@ -32,18 +31,23 @@ namespace PizzaConfigurator
                     default:
                         continue;
                 }
+
                 continue;
             }
         }
 
-        public void showFavoritePizza()
+        public static void showFavoritePizza()
+        {
+        }
+
+        public static void saveToFavorite()
         {
             
         }
 
-        public int keyInput()
+        public static int keyInput()
         {
-            ConsoleKey key = Console.ReadKey(true).Key; 
+            ConsoleKey key = Console.ReadKey(true).Key;
             switch (key)
             {
                 case ConsoleKey.NumPad1:
@@ -64,7 +68,7 @@ namespace PizzaConfigurator
                     return 8;
                 case ConsoleKey.NumPad9:
                     return 9;
-                
+
                 case ConsoleKey.D1:
                     return 1;
                 case ConsoleKey.D2:
@@ -88,11 +92,18 @@ namespace PizzaConfigurator
             }
         }
     }
+
     internal class Pizza
     {
+        public Pizza(bool zaklad = false, bool syrovyZaklad = false)
+        {
+            this.zaklad = zaklad;
+            this.syrovyZaklad = syrovyZaklad;
+        }
+        
         private Boolean zaklad; //case false tomato, case true smetana
         private Boolean syrovyZaklad;
-
+        
         public bool Zaklad
         {
             get => zaklad;
@@ -104,147 +115,240 @@ namespace PizzaConfigurator
             get => syrovyZaklad;
             set => syrovyZaklad = value;
         }
-
-        struct Syry
-        {
-            private Boolean mozzarela, eidam, hermelin, parmazan;//sýry
-
-            public bool Mozzarela
+        
+        public struct Syry{
+            private static Boolean mozzarela, eidam, hermelin, parmazan; //sýry
+            public static bool Mozzarela
             {
                 get => mozzarela;
                 set => mozzarela = value;
             }
 
-            public bool Eidam
+            public static bool Eidam
             {
                 get => eidam;
                 set => eidam = value;
             }
 
-            public bool Hermelin
+            public static bool Hermelin
             {
                 get => hermelin;
                 set => hermelin = value;
             }
 
-            public bool Parmazan
+            public static bool Parmazan
             {
                 get => parmazan;
                 set => parmazan = value;
             }
-        }
+            }
 
-        struct Uzeniny
+        public struct Uzeniny
         {
-            private Boolean sunka, slanina, vysocina, klobaska;//uzeniny
+            private static Boolean sunka, slanina, vysocina, klobaska; //uzeniny
 
-            public bool Sunka
+            public static bool Sunka
             {
                 get => sunka;
                 set => sunka = value;
             }
 
-            public bool Slanina
+            public static bool Slanina
             {
                 get => slanina;
                 set => slanina = value;
             }
 
-            public bool Vysocina
+            public static bool Vysocina
             {
                 get => vysocina;
                 set => vysocina = value;
             }
 
-            public bool Klobaska
+            public static bool Klobaska
             {
                 get => klobaska;
                 set => klobaska = value;
             }
         }
 
-        struct Maso
+        public struct Maso
         {
-            private Boolean kureci, veprove;//maso
+            private static Boolean kureci, veprove; //maso
 
-            public bool Kureci
+            public static bool Kureci
             {
                 get => kureci;
                 set => kureci = value;
             }
 
-            public bool Veprove
+            public static bool Veprove
             {
                 get => veprove;
                 set => veprove = value;
             }
         }
 
-        struct Ostatni
+        public struct Ostatni
         {
-            private Boolean kukurice, zampiony, feferonky, ananas, vejce, olivy;//ostatní
+            private static Boolean kukurice, zampiony, feferonky, ananas, vejce, olivy; //ostatní
 
-            public bool Kukurice
+            public static bool Kukurice
             {
                 get => kukurice;
                 set => kukurice = value;
             }
 
-            public bool Zampiony
+            public static bool Zampiony
             {
                 get => zampiony;
                 set => zampiony = value;
             }
 
-            public bool Feferonky
+            public static bool Feferonky
             {
                 get => feferonky;
                 set => feferonky = value;
             }
 
-            public bool Ananas
+            public static bool Ananas
             {
                 get => ananas;
                 set => ananas = value;
             }
 
-            public bool Vejce
+            public static bool Vejce
             {
                 get => vejce;
                 set => vejce = value;
             }
 
-            public bool Olivy
+            public static bool Olivy
             {
                 get => olivy;
                 set => olivy = value;
             }
         }
-        
-        public void selectBase()
+
+        public void Phase(int phase)
         {
-            Console.WriteLine("Vyberte si základ");
-            
+            switch (phase)
+            {
+                case 1:
+                    selectZaklad();
+                    break;
+                case 2:
+                    selectSyrovyZaklad();
+                    break;
+                case 3:
+                    selectSyry();
+                    break;
+                case 4:
+                    selectUzeniny();
+                    break;
+                case 5:
+                    selectMaso();
+                    break;
+                case 6:
+                    selectExtra();
+                    break;
+            }
+        }
+        public bool selectZaklad()
+        {
+            Console.WriteLine("Vyberte si základ:");
+            Console.WriteLine("1 = Rajčatový základ");
+            Console.WriteLine("2 = Smetanový základ");
+            Console.WriteLine("3 = Zpět");
+            while (true)
+            {
+                if(Program.keyInput()==1)
+                {
+                    return zaklad == true;
+                }else if (Program.keyInput() == 2)
+                {
+                    return zaklad == false;
+                }
+                else if (Program.keyInput() == 3)
+                {
+                    Program.Start();
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
         
+        public bool selectSyrovyZaklad()
+        {
+            Console.WriteLine("Sýrový základ:");
+            Console.WriteLine("1 = Ano");
+            Console.WriteLine("2 = Ne");
+            Console.WriteLine("3 = Zpět");
+            while (true)
+            {
+                if(Program.keyInput()==1)
+                {
+                    return SyrovyZaklad == true;
+                }else if (Program.keyInput() == 2)
+                {
+                    return SyrovyZaklad == false;
+                }
+                else if (Program.keyInput() == 3)
+                {
+                    Phase(0);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
         public void selectSyry()
         {
-            
+            while (true)
+            {
+                Console.WriteLine("Sýry:");
+                Console.WriteLine("1 = Mozzarela " + Syry.Mozzarela);
+                Console.WriteLine("2 = Mozzarela " + Syry.Mozzarela);
+                Console.WriteLine("3 = Mozzarela " + Syry.Mozzarela);
+                Console.WriteLine("4 = Mozzarela " + Syry.Mozzarela);
+                Console.WriteLine("5 = Zpět");
+                switch (Program.keyInput())
+                {
+                    case 1:
+                        Syry.Mozzarela = !Syry.Mozzarela;
+                        continue;
+                    case 2:
+                        Syry.Hermelin = !Syry.Hermelin;
+                        continue;
+                    case 3:
+                        Syry.Eidam = !Syry.Eidam;
+                        continue;
+                    case 4:
+                        Syry.Parmazan = !Syry.Parmazan;
+                        continue;
+                    case 5:
+                        Phase(1);
+                        break;
+                    default:
+                        continue;
+                }
+            }
         }
 
         public void selectUzeniny()
         {
-            
         }
 
         public void selectMaso()
         {
-            
         }
 
         public void selectExtra()
         {
-            
         }
     }
 }
