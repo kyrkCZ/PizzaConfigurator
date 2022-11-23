a39c1b279e5a9e517aa35ea2431b3ff75e132e06
kyrkCZ <kramny.kubik@seznam.cz>
2022-11-24 00:13:11 +0100

    almost completed ShowDefaultPizza

--- PizzaConfigurator/Program.cs
+++ PizzaConfigurator/Program.cs
@@ -11,9 +11,11 @@ namespace PizzaConfigurator
 {
     class Program
     {
+        private static readonly string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+        const string ordersPath = @"c:\temp\orders.json";
+        const string favoritePath = @"c:\temp\favorites.json";
         public static void Main(string[] args)
         {
-            
             Program program = new Program();
             program.Start();
         }
@@ -57,14 +59,61 @@ namespace PizzaConfigurator
         private void ShowDefaultPizza()
         {
             Console.Clear();
-            Console.WriteLine("------------Pizza konfigurátor------------");
-            Console.WriteLine("1 = Sestavení nové pizza");
-            Console.WriteLine("2 = Seznam oblíbených pizza");
-            Console.WriteLine("3 = Výběr ze základních pizza");
-            Console.WriteLine("4 = Odejít");
+            Console.WriteLine("------------Základní pizzy------------");
+            Console.WriteLine("1 = Zpátky");
+            string strWorkPath = System.IO.Path.GetDirectoryName(exePath) + "\\PizzaTypes";
+            DirectoryInfo directoryInfo = new DirectoryInfo(strWorkPath);
+            List<Pizza> defaultPizzas = new List<Pizza>();
+            int counter = 2;
+            foreach (var file in directoryInfo.GetFiles("*.json"))
+            {
+                String fileName = file.Name.Remove(file.Name.IndexOf("."), 5);
+                fileName = fileName.ToUpper();
+                StreamReader sr = File.OpenText(file.FullName);
+                string pizzaString = sr.ReadToEnd();
+                Pizza pizza = JsonConvert.DeserializeObject<Pizza>(pizzaString);
+                defaultPizzas.Add(pizza);
+                Console.WriteLine(counter + " = " + fileName + ": " + pizzaString);
+                counter++;
+            }
+
+            while (true)
+            {
+                switch (KeyInput())
+                {
+                    case 1:
+                        Main(new string[] { });
+                        break;
+                    case 2:
+                        if (defaultPizzas.Count < 1)
+                        {
+                            
+                            break;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                }
+            }
+            
         }
-        
-        public static void SavePizzaToOrders(Pizza pizza)
+
+        private static void SavePizzaToOrders(Pizza pizza)
         {
             string jsonPizza = JsonConvert.SerializeObject(pizza);
             SavePizzaToOrdersFile(jsonPizza);
@@ -73,21 +122,20 @@ namespace PizzaConfigurator
         public static Task SavePizzaToOrdersFile(string jsonPizza)
         {
             string text = jsonPizza;
-            string path = @"c:\temp\orders.json";
             try
             {
                 // Create the file, or overwrite if the file exists.
-                if (File.Exists(path))
+                if (File.Exists(ordersPath))
                 {
-                    var lineCount = File.ReadLines(path).Count();
-                    text = String.Join(" ", File.ReadAllLines(path))+"\n" + lineCount + ": " + text;
+                    var lineCount = File.ReadLines(ordersPath).Count();
+                    text = String.Join(" ", File.ReadAllLines(ordersPath))+"\n" + (lineCount+1) + ": " + text;
                 }
                 else
                 {
                     text = "1: " + text;
                 }
 
-                using (FileStream fs = File.Create(path))
+                using (FileStream fs = File.Create(ordersPath))
                 {
                     byte[] info = new UTF8Encoding(true).GetBytes(text);
                     // Add some information to the file.
@@ -95,7 +143,7 @@ namespace PizzaConfigurator
                 }
 
                 // Open the stream and read it back.
-                using (StreamReader sr = File.OpenText(path))
+                using (StreamReader sr = File.OpenText(ordersPath))
                 {
                     string s = "";
                     while ((s = sr.ReadLine()) != null)
@@ -146,7 +194,7 @@ namespace PizzaConfigurator
                 if (File.Exists(path))
                 {
                     var lineCount = File.ReadLines(path).Count();
-                    text = String.Join(" ", File.ReadAllLines(path))+"\n" + lineCount + ": " + text;
+                    text = String.Join(" ", File.ReadAllLines(path))+"\n" + (lineCount+1) + ": " + text;
                 }
                 else
                 {
@@ -177,14 +225,47 @@ namespace PizzaConfigurator
             }
             return Task.CompletedTask;
         }
-
         
         public void ShowFavoritePizza()
         {
-            List<Pizza> fPizzas = new List<Pizza>();
-            
+            Console.Clear();
+            Console.WriteLine("------------Oblébené pizzy------------");
+            Console.WriteLine("1 = Zpět");
+            if (File.Exists(favoritePath))
+            {
+                List<Pizza> favoritePizzas = new List<Pizza>();
+
+                int counter = 0;  
+                
+                foreach (string line in System.IO.File.ReadLines(favoritePath))
+                {
+                    string[] lineArray = line.ToCharArray().Select(c => c.ToString()).ToArray();
+                    string json = null;
+                    for (int i = 3; i < line.Length; i++)
+                    {
+                        json += lineArray[i];
+                    }
+                    Pizza pizza = JsonConvert.DeserializeObject<Pizza>(json);
+                    favoritePizzas.Add(pizza);
+                    counter++;
+                    Console.WriteLine(counter + ": " + json);
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Nemáte žádné oblíbené pizzy");
+            }
+            while (true)
+            {
+                int keyInput = KeyInput();
+                if (keyInput == 1)
+                {
+                    Program.Main(new string[] { });
+                }
+            }
         }
-        
+
         public static void Phase(int phase, Pizza pizza)
         {
             switch (phase)
